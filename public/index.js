8a4260059a009b5011d07c47c5c2d0f472b0ed42
Billy Angermeier <william.r.angermeier@gmail.com>
2018-07-11 16:23:35 -0400

    worked on JWT

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+const express
+
 $('form').submit(function(event){
     event.preventDefault();
 });
--- users/router.js
+++ users/router.js
@@ -9,6 +9,10 @@ const router = express.Router();
 
 const jsonParser = bodyParser.json();
 
+const requiredFields = ['username', 'password'];
+const missingField = requiredFields.find(field => !(field in req.body));
+
+//ensures required fields are filled
 if (missingField) {
     return res.status(422).json({
       code: 422,
@@ -23,6 +27,7 @@ if (missingField) {
     field => field in req.body && typeof req.body[field] !== 'string'
   );
 
+  //make sure fields entered are strings
   if (nonStringField) {
     return res.status(422).json({
       code: 422,
@@ -32,6 +37,99 @@ if (missingField) {
     });
   }
 
-router.post((req,res)=>{
-      
-})
+  //makes sure no whitespace is at the beginning or end
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  //username must exist, cannot be blank
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+
+    //password must be at least 10, cannot be larger than 72
+    password: {
+      min: 10,
+
+      max: 72
+    }
+  };
+
+  //make sure fields given are appropriate length
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+            req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+            req.body[field].trim().length > sizedFields[field].max
+  );
+
+
+//returns error if field lengths do not fall in parameters
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField]
+          .min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField]
+          .max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+
+  //builds body for creating a new user
+  let {username, password, firstName = '', lastName = ''} = req.body;
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+
+  //ensures username is not already taken
+  return User.find({username})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+
+      //hashes the password for new user
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+    .catch(err => {
+
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({code: 500, message: 'Internal server error'});
+    });
